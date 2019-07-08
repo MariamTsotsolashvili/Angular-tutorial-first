7e0b0162d06d95a55c7f34f6e7a8b21e0b52a3d6
MariamTsotsolashvili <mariami.tsotsolashvili.1@btu.edu.ge>
2019-07-08 18:27:07 +0400

    feat(10-form-validation-advanced-cross-field-validation-part-2): Finish tutorial

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,6 +1,6 @@
 import { CartService } from './../cart.service';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators, FormControl } from '@angular/forms';
+import { FormBuilder, Validators, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
     selector: 'app-cart',
@@ -22,16 +22,39 @@ export class CartComponent implements OnInit {
                 city: '',
                 state: '',
                 zipcode: ''
+            }, {
+                validators: this.crossValidation
             })
         });
     }
 
+    static isZipOk(zip) {
+        return zip.length < 3;
+    }
+
+    static isCityOk(city) {
+        return city.charAt(0).toLowerCase() === 'a';
+    }
+
     ngOnInit() { }
+
+    crossValidation(formGroup) {
+        const zip = formGroup.get('zip').value;
+        const zipStatus = CartComponent.isZipOk(zip);
+
+        const city = formGroup.get('city').value;
+        const cityStatus = CartComponent.isCityOk(city);
+
+        return zipStatus && cityStatus ? null : {
+            zipStatus,
+            cityStatus
+        };
+    }
 
     forbiddenName() {
         return (formControl) => {
             return formControl.value === 'Roman' ? {forbidden: {invalid: true}} : null;
-        }
+        };
     }
     RemoveFromCart(productId) {
         this.cartService.RemoveFromCart(productId);
@@ -50,5 +73,8 @@ export class CartComponent implements OnInit {
     }
     get name() {
         return this.checkedForm.get('name') as FormControl;
+    }
+    get address() {
+        return this.checkedForm.get('address') as FormGroup;
     }
 }
