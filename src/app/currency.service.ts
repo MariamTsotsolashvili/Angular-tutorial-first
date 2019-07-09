e9b18499d6308d7984ad0ab8ec529beea79894c4
MariamTsotsolashvili <mariami.tsotsolashvili.1@btu.edu.ge>
2019-07-09 18:57:39 +0400

    feat(14-observable-filters): Finish tutorial-2

--- src/app/currency.service.ts
+++ src/app/currency.service.ts
@@ -7,20 +7,46 @@ import {data} from './rate';
 export class CurrencyService {
 
   result;
+  Observer;
 
   constructor() {
-    this.result = data.rates;
+    this.result = this.transformObjectToArray(data.rates);
+    this.Observer = {
+      filter: this.filter,
+      map: this.map,
+      subscribe: this.subscribe,
+      result: this.result
+    };
   }
-  subscribe(next, complete) {
-    const keys = Object.keys(this.result);
-    let i = 0;
 
+  transformObjectToArray(object) {
+    const result = [];
+    const keys = Object.keys(object);
     for (const key of keys) {
-      const value = this.result[key];
+      const value = object[key];
       const item = {
         currency: key,
         value
       };
+      result.push(item);
+    }
+    return result;
+  }
+
+  filter(cb) {
+    this.result = this.result.filter(cb);
+    return this;
+  }
+
+  map(cb) {
+    this.result = this.result.map(cb);
+    return this;
+  }
+
+  subscribe(next, complete) {
+    let i = 0;
+
+    for (const item of this.result) {
       setTimeout(() => {
         next(item);
       }, i * 500);
@@ -28,7 +54,7 @@ export class CurrencyService {
       i++;
     }
     setTimeout(() => {
-      complete(keys.length);
+      complete(this.result.length);
     }, i * 500);
   }
 }
