07399e4f98d07d1d7ecb3bac1ec0cea9ef64a518
MariamTsotsolashvili <mariami.tsotsolashvili.1@btu.edu.ge>
2019-07-10 18:40:50 +0400

    feat(19-di-part-2): Finish tutorial-2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,6 @@ import { WishlistComponent } from './wishlist/wishlist.component';
 import { ReactiveFormsModule} from '@angular/forms';
 import { CurrencyComponent } from './currency/currency.component';
 import { ExchangeComponent } from './exchange/exchange.component';
-import { ShippingService } from './shipping.service';
 
 @NgModule({
   declarations: [
@@ -45,7 +44,7 @@ import { ShippingService } from './shipping.service';
     ]),
     ReactiveFormsModule
   ],
-  providers: [ShippingService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
