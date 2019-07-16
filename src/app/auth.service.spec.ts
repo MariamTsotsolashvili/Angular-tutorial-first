4ff1f0597f14bc79665b9d11c09bb49c4d067e69
MariamTsotsolashvili <mariami.tsotsolashvili.1@btu.edu.ge>
2019-07-16 18:02:36 +0400

    feat(27-testing-service): Finish tutorial about testing service

---
+++ src/app/auth.service.spec.ts
@@ -0,0 +1,23 @@
+import { AuthService } from './auth.service';
+
+describe('Auth Service tests', () => {
+    let service: AuthService = new AuthService();
+    beforeEach(() => {
+        service = new AuthService();
+    });
+
+    it('Default value of access should be false', () => {
+        expect(service.isEnabled()).toBe(false);
+    });
+    it('Method allow should set access to true', () => {
+        service.allow();
+        expect(service.isEnabled()).toBe(true);
+    });
+    it('Method disallow should set access to true', () => {
+        service.disallow();
+        expect(service.isEnabled()).toBe(false);
+    });
+    it('Method isEnabled should return access value', () => {
+        expect(service.isEnabled()).toBe(service.access);
+    });
+});
