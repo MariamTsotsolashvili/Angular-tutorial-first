4015e95a7fa06fa54480ff04598c3d855d1e7dab
MariamTsotsolashvili <mariami.tsotsolashvili.1@btu.edu.ge>
2019-07-16 17:02:53 +0400

    feat(26-animations): Fix stuff

--- src/app/employee-register/employee-register.component.ts
+++ src/app/employee-register/employee-register.component.ts
@@ -1,16 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { EmployeesService } from '../employees.service';
+import {trigger, state, style, transition, animate} from '@angular/animations';
 
 
 @Component({
   selector: 'app-employee-register',
   templateUrl: './employee-register.component.html',
-  styleUrls: ['./employee-register.component.scss']
+  styleUrls: ['./employee-register.component.scss'],
+  animations: [
+    trigger('showHide', [
+      state('show', style({
+        left: '600px'
+      })),
+      state('hide', style({
+        left: '900px'
+      })),
+      transition('show <=> hide', [
+        animate('0.2s')
+      ])
+    ])
+  ]
 })
 export class EmployeeRegisterComponent implements OnInit {
   employeeregister;
   popCheck = false;
+  visible = false;
   constructor(private formBuilder: FormBuilder, private employeesService: EmployeesService ) {
     this.employeeregister = formBuilder.group( {
       name: [''],
@@ -25,5 +40,6 @@ export class EmployeeRegisterComponent implements OnInit {
   }
   popUp() {
     this.popCheck = !this.popCheck;
+    this.visible = true;
   }
 }
