55d75d07a9cb0c8f69da81846a096fcdd565953d
MariamTsotsolashvili <mariami.tsotsolashvili.1@btu.edu.ge>
2019-07-16 17:43:01 +0400

    feat(finish task 1):

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -1,20 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeesService } from '../employees.service';
+import { trigger, state, style } from '@angular/animations';
 
 
 @Component({
  selector: 'app-employees',
  templateUrl: './employees.component.html',
  styleUrls: ['./employees.component.scss'],
- animations: []
 })
 export class EmployeesComponent implements OnInit {
  employees;
- isLoaded = false;
- myModal = false;
+ loader = true;
  constructor(private employeesService: EmployeesService ) { }
-
  ngOnInit() {
    this.employees = this.employeesService.getEmployees();
+   this.employees.subscribe(data => {
+     this.loader = false;
+   });
  }
+
 }
